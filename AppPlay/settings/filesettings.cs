3db0917904d3a43c271c97d28b8e01184e72cf45
ahmedmoamadan <ahmed.mohamed.ramadan010@gmail.com>
2025-04-17 15:42:46 +0200

    edit some header

--- AppPlay/settings/filesettings.cs
+++ AppPlay/settings/filesettings.cs
@@ -3,7 +3,7 @@
     public static class filesettings
     {
         public const string ImagePath = "/assets/Image/Games";
-        public const string AllowExtentions = ".jpg,.gpeg,.jpng";
+        public const string AllowExtentions = ".jpg,.jpeg,.jpng";
         public const int maxSizeInMB = 5;
         public const int maxSizeByByets = maxSizeInMB * 1024 *1024;
     }
